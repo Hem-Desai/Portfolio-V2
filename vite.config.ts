fe6db2638026abf903a718fa656db32003d7e9fa
Hem-Desai <hemniraldesai@gmail.com>
2025-04-04 15:47:05 +0530

    chalo bhai please

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",
   optimizeDeps: {
     exclude: ["lucide-react"],
-  },
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-    sourcemap: true,
   },
 });
